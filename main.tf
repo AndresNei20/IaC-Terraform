0eabea0f3b9ff5c34a0d1d55d335838f743aa509
Andres Neira <Andres.Neira@endava.com>
2026-04-29 11:33:03 -0500

    Add Security group to allow HTTP

--- main.tf
+++ main.tf
@@ -1,6 +1,27 @@
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP access"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t3.micro"
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
